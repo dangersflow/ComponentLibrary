a0dc0923383f18142ddd36d6f02e24be63f1b744
Frank <pacog879@hotmail.com>
2022-12-27 20:00:08 -0600

    finished v1 of dropdown; started listbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import "./styles.css";
 import Popover from "./components/Popover";
+import PopoverItem from "./components/PopoverItem";
+import { BiLogIn } from "react-icons/bi";
+import { GiArchiveRegister } from "react-icons/gi";
+import { AiFillDelete } from "react-icons/ai";
 
 export default function App() {
   return (
-    <Popover buttonLabel={"Dropdrown Menu"}>
-      <h1> Hello </h1>
-      <h1> I am h1 </h1>
-      <h1> Element </h1>
+    <Popover buttonLabel={"Dropdown Menu"} iconOrientation={"right"}>
+      <PopoverItem
+        icon={<BiLogIn size={30} />}
+        title={"Login"}
+        subtitle={"Login to your account"}
+      />
+      <PopoverItem
+        icon={<GiArchiveRegister size={30} />}
+        title={"Register"}
+        subtitle={"Register for an account"}
+      />
+      <PopoverItem
+        icon={<AiFillDelete size={30} />}
+        title={"Delete"}
+        subtitle={"Delete your account"}
+      />
     </Popover>
   );
 }
--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,86 +1,135 @@
 import { Popover } from "@headlessui/react";
 import { RxChevronDown } from "react-icons/rx";
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion/dist/framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+
+const orientationButtonMap = new Map<string, string>([
+  ["left", "row-reverse"],
+  ["right", "row"],
+  ["top", "column-reverse"],
+  ["bottom", "column"],
+]);
 
 const PopoverButton = styled(motion.button)`
   display: flex;
-  flex-direction: row;
+  flex-direction: ${({ iconOrientation }: { iconOrientation: string }) =>
+    orientationButtonMap.get(iconOrientation) || "row"};
   width: auto;
   font-size: 1.5em;
-  gap: 1.5em;
+  color: #8ecae6;
+  gap: 0.5em;
   padding: 0.5em 1em 0.5em 1em;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
-  border: solid black 1px;
+  border: none;
+  background-color: #023047;
 `;
 
 const PopoverPanel = styled(motion.div)`
   display: flex;
   flex-direction: column;
-  border: solid black 1px;
-  width: 20em;
   border-radius: 10px;
-  gap: 10px;
   margin-top: 0.5em;
+  position: absolute;
+  background-color: #023047;
+  color: #8ecae6;
+  min-width: 350px;
+  padding: 0.5em;
 `;
 
 const PopoverItem = styled(motion.div)`
   display: flex;
-  margin-left: 1.5em;
+  border-radius: 10px;
+  margin: 0.5em;
+  padding: 0.5em 1.5em 0.5em 1em;
 `;
 
 interface PopoverProps {
   buttonLabel: string;
   children: JSX.Element[];
+  iconOrientation?: "left" | "right" | "top" | "bottom";
 }
 
-export default function MyPopover({ buttonLabel, children }: PopoverProps) {
+export default function MyPopover({
+  buttonLabel,
+  children,
+  iconOrientation,
+}: PopoverProps) {
   return (
     <Popover>
-      {({ open }) => (
+      {({ open, close }) => (
         /* Use the `open` state to conditionally change the direction of the chevron icon. */
         <>
-          <Popover.Button as={PopoverButton}>
+          <Popover.Button
+            iconOrientation={iconOrientation}
+            as={PopoverButton}
+            whileTap={{
+              scale: 0.98,
+              transition: { duration: 0.1, type: "spring" },
+            }}
+          >
             {buttonLabel}
             <motion.div
               initial={{
-                rotateZ: 0
+                rotateZ: 0,
               }}
               animate={{
-                rotateZ: open ? 180 : 0
+                rotateZ: open ? 180 : 0,
               }}
               transition={{
-                duration: 0.5,
-                type: "spring"
+                duration: 0.2,
+                type: "spring",
               }}
             >
               <RxChevronDown size={"1.5em"} />
             </motion.div>
           </Popover.Button>
-          <AnimatePresence initial={false}>
+          <AnimatePresence exitBeforeEnter>
             {open && (
               <Popover.Panel
-                key="modal"
+                static
+                key={"panel"}
                 as={PopoverPanel}
                 initial={{
                   y: -50,
-                  opacity: 0
+                  opacity: 0,
                 }}
                 animate={{
                   y: 0,
-                  opacity: 1
+                  opacity: 1,
                 }}
-                exit={{ opacity: 0 }}
+                exit={{
+                  opacity: 0,
+                  y: -50,
+                  transition: { duration: 0.1 },
+                }}
                 transition={{
-                  duration: 0.75,
+                  duration: 0.5,
                   type: "spring",
-                  bounce: 0.5
+                  bounce: 0.5,
                 }}
               >
-                {children.map((childComponent) => {
-                  return <PopoverItem>{childComponent}</PopoverItem>;
+                {children.map((childComponent, index) => {
+                  return (
+                    <PopoverItem
+                      key={index}
+                      whileHover={{
+                        backgroundColor: "#8ecae638",
+                        transition: { duration: 0.2, type: "spring" },
+                        cursor: "pointer",
+                      }}
+                      whileTap={{
+                        scale: 0.95,
+                        transition: { duration: 0.1, type: "spring" },
+                      }}
+                      onTap={() => {
+                        close();
+                      }}
+                    >
+                      {childComponent}
+                    </PopoverItem>
+                  );
                 })}
               </Popover.Panel>
             )}
